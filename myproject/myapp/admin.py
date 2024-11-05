686a01af8225385a861c22ae0479f10aa6f44d43
Nina <nlange@st.swps.edu.pl>
2024-11-05 15:09:45 +0100

    lab05

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Stanowisko, Osoba
+
+@admin.register(Stanowisko)
+class StanowiskoAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'opis')
+
+@admin.register(Osoba)
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ('imie', 'nazwisko', 'plec', 'stanowisko_id_display', 'data_dodania')
+    readonly_fields = ('data_dodania',)
+
+    @admin.display(description='Stanowisko (id)')
+    def stanowisko_id_display(self, obj):
+        return f"{obj.stanowisko} ({obj.stanowisko.id})"
+
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Stanowisko(models.Model):
+    nazwa = models.TextField(blank=False, null=False)
+    opis = models.TextField(blank=True)
+
+class Osoba(models.Model):
+    imie = models.TextField(blank=False, null=False)
+    nazwisko = models.TextField(blank=False, null=False)
+    plec_wybor = (
+        ("K", "Kobieta"),
+        ("M", "Męczyzna "),
+        ("I", "Inne")
+    )
+    plec = models.CharField(max_length=1, choices=plec_wybor)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+
+    data_dodania= models.DateField(auto_now=False, auto_now_add=True)
+
+
+def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+class Meta:
+        ordering = ['nazwisko']
